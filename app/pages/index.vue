05bafe316a9371cd2d17da4a1c2c9102cbe8bc3c
ludoguenet <ludovicguenet@gmx.com>
2025-02-14 23:39:19 +0100

    initialize Nuxt.js application with essential configurations and components

---
+++ app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 h-screen">
+    <h1 class="font-bold text-2xl text-(--ui-primary)">
+        Code. Create. Inspire.
+    </h1>
+
+    <div class="flex items-center gap-2">
+      <UButton
+        label="YouTube"
+        icon="i-lucide-square-play"
+        to="https://www.youtube.com/@LaravelJutsu"
+        target="_blank"
+      />
+
+      <UButton
+        label="GitHub"
+        color="neutral"
+        variant="outline"
+        icon="i-simple-icons-github"
+        to="https://github.com/ludoguenet"
+        target="_blank"
+      />
+    </div>
+  </div>
+</template>
