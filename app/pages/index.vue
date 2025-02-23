1c9c0aecffb57e26fb324d97c9588532d1001c70
ludoguenet <ludovicguenet@gmx.com>
2025-02-23 19:09:06 +0100

    feat: add Tailwind CSS integration and update homepage layout with logo and tooltips

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="flex flex-col items-center justify-center gap-4 h-screen">
-    <h1 class="font-bold text-2xl text-(--ui-primary)">
+    <div class="flex flex-col items-center justify-center gap-4 h-screen">
+      <img src="/img/logo.svg" alt="Logo" class="w-72">
+
+      <h1 class="font-bold text-xl text-(--ui-primary)">
         Code. Create. Inspire.
-    </h1>
+      </h1>
 
-    <div class="flex items-center gap-2">
-      <UButton
-        label="YouTube"
-        icon="i-lucide-square-play"
-        to="https://www.youtube.com/@LaravelJutsu"
-        target="_blank"
-      />
+      <div class="flex items-center gap-2">
+        <UTooltip text="Watch my videos on YouTube">
+            <UButton
+            label="YouTube"
+            icon="i-lucide-square-play"
+            to="https://www.youtube.com/@LaravelJutsu"
+            target="_blank"
+            />
+        </UTooltip>
 
-      <UButton
-        label="GitHub"
-        color="neutral"
-        variant="outline"
-        icon="i-simple-icons-github"
-        to="https://github.com/ludoguenet"
-        target="_blank"
-      />
+        <UTooltip text="See my code on GitHub">
+            <UButton
+            label="GitHub"
+            color="neutral"
+            variant="outline"
+            icon="i-simple-icons-github"
+            to="https://github.com/ludoguenet"
+            target="_blank"
+            />
+        </UTooltip>
+      </div>
     </div>
-  </div>
-</template>
+  </template>
