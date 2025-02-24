db3346486ef5ef99b6071f55a9a2c65fd5581562
ludoguenet <ludovicguenet@gmx.com>
2025-02-24 10:54:21 +0100

    feat: enhance homepage layout with improved spacing, typography, and tooltip delays

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,30 +1,34 @@
 <template>
-    <div class="flex flex-col items-center justify-center gap-4 h-screen">
-      <img src="/img/logo.svg" alt="Logo" class="w-72">
+    <div class="flex flex-col items-center justify-center gap-6 h-screen p-4">
+      <img src="/img/logo.svg" alt="Logo" class="w-60 h-auto">
 
-      <h1 class="font-bold text-xl text-(--ui-primary)">
+      <h1 class="font-bold text-2xl sm:text-3xl text-(--ui-primary) tracking-wide">
         Code. Create. Inspire.
       </h1>
 
       <div class="flex items-center gap-2">
-        <UTooltip text="Watch my videos on YouTube">
-            <UButton
+        <UTooltip text="Watch my videos on YouTube" delay="200">
+          <UButton
             label="YouTube"
             icon="i-lucide-square-play"
             to="https://www.youtube.com/@LaravelJutsu"
             target="_blank"
-            />
+            size="lg"
+            class="rounded-lg hover:shadow-md transition duration-300"
+          />
         </UTooltip>
 
-        <UTooltip text="See my code on GitHub">
-            <UButton
+        <UTooltip text="See my code on GitHub" delay="200">
+          <UButton
             label="GitHub"
             color="neutral"
             variant="outline"
             icon="i-simple-icons-github"
             to="https://github.com/ludoguenet"
             target="_blank"
-            />
+            size="lg"
+            class="rounded-lg hover:shadow-md transition duration-300"
+          />
         </UTooltip>
       </div>
     </div>
