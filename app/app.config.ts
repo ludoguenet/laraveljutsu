f2148caa0ee86fc8fa6ca00ade9f1a9b001ce655
ludoguenet <ludovicguenet@gmx.com>
2025-03-16 22:36:26 +0100

    fix: update Tailwind CSS import and adjust theme colors in app configuration

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,8 +2,8 @@ export default defineAppConfig({
   // https://ui3.nuxt.dev/getting-started/theme#design-system
   ui: {
     colors: {
-      primary: 'zinc',
-      neutral: 'slate',
+      primary: 'lime',
+      neutral: 'zinc',
     },
     button: {
       defaultVariants: {
