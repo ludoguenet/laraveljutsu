05fe324c61ae4743ac2586004fadd3f951c6761b
ludoguenet <ludovicguenet@gmx.com>
2025-05-10 23:03:52 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,7 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/eslint',
-    '@nuxtjs/color-mode',
-    '@nuxt/content'
-  ],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxtjs/color-mode', '@nuxt/content', '@nuxt/icon'],
 
   app: {
     head: {
